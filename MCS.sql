9f34b8e8af294d244913dc6257d9c6e0c18a619e
Jenia-Castillo <jenia.castillo@utp.ac.pa>
2022-07-03 13:27:51 -0500

    Update MCS.sql
    el servicio estaba en int no en varchar xd

--- MCS.sql
+++ MCS.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `preguntasfrecuentes` (
 -- Volcando estructura para tabla mcs.servicios
 CREATE TABLE IF NOT EXISTS `servicios` (
   `id_servicio` int(11) NOT NULL AUTO_INCREMENT,
-  `servicio` INT(11) NOT NULL,
+  `servicio` VARCHAR(50) NOT NULL COLLATE 'latin1_swedish_ci',
   `costo` float NOT NULL,
   `descripcion` text NOT NULL,
   PRIMARY KEY (`id_servicio`)
